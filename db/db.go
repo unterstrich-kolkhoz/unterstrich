e09f0b676cfb13fadc8b1a4c8079f2e192966b57
hellerve <veit@veitheller.de>
2018-01-30 16:29:47 +0100

    db: simplify and document

--- db/db.go
+++ db/db.go
@@ -2,15 +2,11 @@ package db
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // this is needed by Gorm
 )
 
+// Create creates a database instance using a dialect and database name
 func Create(dialect string, dbname string) (*gorm.DB, error) {
-	db, err := gorm.Open(dialect, dbname)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	// for now
+	return gorm.Open(dialect, dbname)
 }
